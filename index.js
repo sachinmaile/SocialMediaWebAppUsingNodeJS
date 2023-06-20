9fbb73291f66c85406265245d764034f34ecda6f
sachinmaile <108009218+sachinmaile@users.noreply.github.com>
2023-06-20 19:54:06 +0530

    Creating a Layout

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express=require('express');
+const expressLayouts=require('express-ejs-layouts');
 const path=require('path');
 const port=8000;
 const app=express();
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
+
+app.use(express.urlencoded());
+app.use(expressLayouts);
 
 app.get('/',function(req,res){
     return res.render('home',{title:"Social Media Web App"});
