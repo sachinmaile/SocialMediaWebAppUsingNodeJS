1fa5527cbcf92c2ea87faaddd16d5619868d9e18
sachinmaile <108009218+sachinmaile@users.noreply.github.com>
2023-06-22 08:54:37 +0530

    Display post and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,6 @@
 const Post=require('../models/post');
 module.exports.home=async (req,res)=>{
-    await Post.find({})
+    await Post.find({}).populate('user').exec()
     .then((posts)=>{
         return res.render('home',{title:'SocialMediaWebApp|Home',posts:posts});
     });
