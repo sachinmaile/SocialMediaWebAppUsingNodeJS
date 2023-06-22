a73990214e8fce465767c9b226d8d6654bc2a474
sachinmaile <108009218+sachinmaile@users.noreply.github.com>
2023-06-22 08:32:12 +0530

    Display posts and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,7 @@
-module.exports.home=function(req,res){
-    return res.end('<h1>Express is up for codeial</h1>');
+const Post=require('../models/post');
+module.exports.home=async (req,res)=>{
+    await Post.find({})
+    .then((posts)=>{
+        return res.render('home',{title:'SocialMediaWebApp|Home',posts:posts});
+    });
 }
